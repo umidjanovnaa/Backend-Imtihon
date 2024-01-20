a079bc8df709ac86513fe7bd644bead694fef3b2
umidjanovnaa <sheraliyevaruxshona08@gmail.com>
2024-01-20 13:52:34 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# 1_masala
+list = [1,2,3,4,6,7,8,9,10]
+num =[]
+def son (list , num):
+    for x in range(1 , 10 +1):
+
+
+# 2_masala
+# def car(total_people):
+#     seats_car_people = 5
+#     cars_netoded =((total_people + seats_car_people)//seats_per_car)
+#     return cars_netoded
+#
+# total_people =int(input("Odam sonini kiriting: "))
+# cars = car(total_people)
+#
+# print(f"{total_people}{cars}")
+
+# 3_masala
+# number =int(input('Sonni kiritng: '))
